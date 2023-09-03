ad61eb3c40ebe156ccb8ee911f6e1d0c4856a40d
Egor Evdokimov <vdkmvx@yandex.ru>
2023-09-03 12:52:47 +0300

    Фикстура на логин

--- conftest.py
+++ conftest.py
@@ -1,6 +1,16 @@
 import pytest
 import psycopg2
-from data.getenv import DB, DB_HOST, DB_PORT, DB_PASSWORD, DB_USERNAME
+import requests
+from data.getenv import (
+    DB,
+    DB_HOST,
+    DB_PORT,
+    DB_PASSWORD,
+    DB_USERNAME,
+    HOST,
+    TEST_PASSWORD,
+    TEST_EMAIL,
+)
 
 
 @pytest.fixture()
@@ -17,3 +27,15 @@ def database():
         connection.close()
     except:
         print("Can`t establish connection to database")
+
+
+@pytest.fixture(scope="session")
+def login():
+    _session = requests.Session()
+    print(_session.headers)
+    response = requests.post(
+        HOST + "/auth/login", data={"email": TEST_EMAIL, "password": TEST_PASSWORD}
+    )
+    auth = response.json()["access_token"]
+    _session.headers.update({"Authorization": f"Bearer {auth}"})
+    return _session
